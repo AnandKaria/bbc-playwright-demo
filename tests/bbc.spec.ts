c0b1b9f410aaa9173d5cc158ecc7e9e66e871fdd
AnandKaria <anand.a.karia@gmail.com>
2025-01-14 18:11:09 +0000

    Cleaned up describe box, added weather page test

--- tests/bbc.spec.ts
+++ tests/bbc.spec.ts
@@ -1,16 +1,24 @@
 import { test, expect } from '@playwright/test';
 
+test.beforeEach('Load home page, set cookie status', async ({ page }) => {
+  // Navigate to main page
+  await page.goto('https://www.bbc.co.uk/');
+
+  // Check cookie banner exists, reject non-essential, check banner dismissed
+  await expect(page.getByLabel('Cookies on the BBC website')).toBeVisible();
+  await page.getByTestId('reject-button').click();
+  await expect(page.getByLabel('Cookies on the BBC website')).not.toBeVisible();
+});
+
 test.describe('Open BBC frontpages', () => {
-  test('has key sections for Discover and Most Read', async ({ page }) => {
-    // Navigate to main page
-    await page.goto('https://www.bbc.co.uk/');
+  test('Homepage link works', async ({ page }) => {
+    // Navigate to home
+    await page.getByTestId('header-content').getByRole('link', { name: 'Home', exact: true }).click();
+  });
 
-    // Check cookie banner exists and reject non-essential
-    await expect(page.getByLabel('Cookies on the BBC website')).toBeVisible();
-    await page.getByTestId('reject-button').click();
-
+  test('News page accessible and has key content', async ({ page }) => {
     // Navigate to news
-    await page.getByTestId('header-content').getByRole('link', { name: 'News' }).click();
+    await page.getByTestId('header-content').getByRole('link', { name: 'News', exact: true }).click();
 
     // Check page title
     await expect(page).toHaveTitle(/BBC News/);
@@ -20,7 +28,32 @@ test.describe('Open BBC frontpages', () => {
     await expect(page.getByTestId('sign-in-banner').getByRole('heading')).toContainText('Discover your BBC');
 
     // Validate Most Read section
-    await expect(page.getByText('Most read1Briton, 18,')).toBeVisible();
     await expect(page.getByRole('heading', { name: 'Most read' })).toBeVisible();
+  });
+
+  test('Weather page accessible and has key content', async ({ page }) => {
+    // Navigate to weather link
+    await page.getByTestId('header-content').getByRole('link', { name: 'Weather' }).click();
+
+    // Check page title
+    await expect(page).toHaveTitle(/BBC Weather/);
+
+    // Check page content
+    await expect(page.getByRole('link', { name: 'BBC Weather' })).toBeVisible();
+    await expect(page.getByRole('heading', { name: 'UK Summary' })).toBeVisible();
+
+    // Search for and find Heathrow
+    await page.getByPlaceholder('Enter a town, city or UK').click();
+    await page.getByPlaceholder('Enter a town, city or UK').fill('Heathrow');
+    // await page.getByPlaceholder('Enter a town, city or UK').press('Enter');
+    await page.getByRole('button', { name: 'Search', exact: true }).click();
+    await expect(page.locator('#location-list')).toContainText('London Heathrow Airport, Greater London');
+    await expect(page.getByRole('link', { name: 'London Heathrow Airport,' })).toBeVisible();
+    await page.getByRole('link', { name: 'London Heathrow Airport,' }).click();
+    
+    // Validate weather page for Heathrow
+    await expect(page.getByTestId('location').getByRole('heading', { name: 'London Heathrow Airport' })).toBeVisible();
+    await expect(page.locator('#site-container')).toContainText('Observation Station: London/Heathrow Intl (Lat: 51.4833 | Long: -0.45)');
+    await page.getByLabel('BBC-wide').getByRole('link', { name: 'Home', exact: true }).click();
   });
 });
