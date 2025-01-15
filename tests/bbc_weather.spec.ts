93843f21ce0b137f7de20034d8718a7e8faa6a8a
AnandKaria <anand.a.karia@gmail.com>
2025-01-15 16:52:23 +0000

    [Weather] Modularised out a function for keying the search term and getting a locator

--- tests/bbc_weather.spec.ts
+++ tests/bbc_weather.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test.beforeEach('Load home page, set cookie status', async ({ page }) => {
+test.beforeEach('Load home page, reject cookies, click Weather link', async ({ page }) => {
   // Navigate to main page
   await page.goto('https://www.bbc.co.uk/');
 
@@ -8,6 +8,9 @@ test.beforeEach('Load home page, set cookie status', async ({ page }) => {
   await expect(page.getByLabel('Cookies on the BBC website')).toBeVisible();
   await page.getByTestId('reject-button').click();
   await expect(page.getByLabel('Cookies on the BBC website')).not.toBeVisible();
+
+  // Navigate to weather link
+  await page.getByTestId('header-content').getByRole('link', { name: 'Weather' }).click();
 });
 
 test.describe('Search for weather forecasts - exact match', () => {
@@ -31,20 +34,15 @@ test.describe('Search for weather forecasts - exact match', () => {
     },
   ].forEach(({term, list_entry, heading, obs_station}) => {
     test('Weather page search returns results for ' + term, async ({ page }) => {
-      // Navigate to weather link
-      await page.getByTestId('header-content').getByRole('link', { name: 'Weather' }).click();
-  
-      // Search for term and navigate into link
-      const w_search_header = page.getByTestId('weather-masthead');
-      const w_search_textbox = w_search_header.getByPlaceholder('Enter a town, city or UK');
-      await w_search_textbox.click();
-      await w_search_textbox.pressSequentially(term, { delay: 50 });
-      // await w_search_textbox.press('Enter');
-      await w_search_header.getByRole('button', { name: 'Search', exact: true }).click();
-      await expect(w_search_header.locator('#location-list').getByRole('link', { name: list_entry })).toBeVisible();
-      await w_search_header.getByRole('link', { name: list_entry }).click();
+      // Search for term and get locators
+      const masthead = await input_search_term(page, term);
+
+      // Navigate into link
+      await masthead.getByRole('button', { name: 'Search', exact: true }).click();
+      await expect(masthead.locator('#location-list').getByRole('link', { name: list_entry })).toBeVisible();
+      await masthead.getByRole('link', { name: list_entry }).click();
       
-      // Validate weather page for Heathrow
+      // Validate weather page for location
       await expect(page.getByTestId('location').getByRole('heading', { name: heading })).toBeVisible();
       await expect(page.locator('.wr-c-observations__details')).toContainText(obs_station);
     });
@@ -72,24 +70,19 @@ test.describe('Search for weather forecasts - partial match', () => {
     },
   ].forEach(({term, list_entry, heading, obs_station}) => {
     test('Weather page search returns results for ' + term, async ({ page }) => {
-      // Navigate to weather link
-      await page.getByTestId('header-content').getByRole('link', { name: 'Weather' }).click();
-  
-      // Search for term, get autocompleted result and navigate into link
-      const w_search_header = page.getByTestId('weather-masthead');
-      const w_search_textbox = w_search_header.getByPlaceholder('Enter a town, city or UK');
-      await w_search_textbox.click();
-      await w_search_textbox.pressSequentially(term, { delay: 50 });
-      await expect(w_search_header.locator('#location-list').getByRole('link', { name: list_entry })).toBeVisible();
-      await w_search_header.getByRole('link', { name: list_entry }).click();
+      // Search for term and get locators
+      const masthead = await input_search_term(page, term);
+
+      // Check autocomplete and navigate into link
+      await expect(masthead.locator('#location-list').getByRole('link', { name: list_entry })).toBeVisible();
+      await masthead.getByRole('link', { name: list_entry }).click();
       
-      // Validate weather page for Heathrow
+      // Validate weather page for location
       await expect(page.getByTestId('location').getByRole('heading', { name: heading })).toBeVisible();
       await expect(page.locator('.wr-c-observations__details')).toContainText(obs_station);
     });
   })
 });
-
 
 test.describe('Search for weather forecasts - no match', () => {
   // Set test data
@@ -99,19 +92,15 @@ test.describe('Search for weather forecasts - no match', () => {
     { term: '1234567',},
   ].forEach(({term}) => {
     test('Weather page search doesn\'t return results for ' + term, async ({ page }) => {
-      // Navigate to weather link
-      await page.getByTestId('header-content').getByRole('link', { name: 'Weather' }).click();
-  
-      // Search for term and get no autocompleted results
-      const w_search_header = page.getByTestId('weather-masthead');
-      const w_search_textbox = w_search_header.getByPlaceholder('Enter a town, city or UK');
-      await w_search_textbox.click();
-      await w_search_textbox.pressSequentially(term, { delay: 50 });
-      await expect(w_search_header.locator('#location-list')).not.toBeVisible();
+      // Search for term and get locators
+      const masthead = await input_search_term(page, term);
+
+      // Get no autocompleted results
+      await expect(masthead.locator('#location-list')).not.toBeVisible();
 
       // Click search and check "no results" message
-      await w_search_header.getByRole('button', { name: 'Search', exact: true }).click();
-      const w_search_nfmsg = w_search_header.locator('.ls-c-message__content').first();
+      await masthead.getByRole('button', { name: 'Search', exact: true }).click();
+      const w_search_nfmsg = masthead.locator('.ls-c-message__content').first();
       await w_search_nfmsg.waitFor({ timeout: 2000 });
       await expect(w_search_nfmsg).toContainText('We couldn\'t find any results for "' + term + '"', { timeout: 5000 });
 
@@ -120,3 +109,11 @@ test.describe('Search for weather forecasts - no match', () => {
     });
   })
 });
+
+async function input_search_term(page, term) {
+  const masthead = page.getByTestId('weather-masthead');
+  const textbox = page.getByTestId('weather-masthead').getByPlaceholder('Enter a town, city or UK');
+  await textbox.click();
+  await textbox.pressSequentially(term, { delay: 50 });
+  return masthead;
+}
