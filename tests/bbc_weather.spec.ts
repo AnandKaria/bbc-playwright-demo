13d94f451e57d83a02f2a09c887a12e8cba026b3
AnandKaria <anand.a.karia@gmail.com>
2025-02-04 23:32:52 +0000

    [Weather] Configured full match tests to source data from CSV file

--- tests/bbc_weather.spec.ts
+++ tests/bbc_weather.spec.ts
@@ -1,3 +1,6 @@
+import fs from 'fs';
+import path from 'path';
+import { parse } from 'csv-parse/sync';
 import { test, expect, type Page } from '@playwright/test';
 
 test.beforeEach('Load home page, reject cookies, click Weather link', async ({ page }) => {
@@ -13,40 +16,36 @@ test.beforeEach('Load home page, reject cookies, click Weather link', async ({ p
   await page.getByTestId('header-content').getByRole('link', { name: 'Weather' }).click();
 });
 
+
 test.describe('Search for weather forecasts - exact match', () => {
-  // Set test data
-  // - term: term to be searched in masthead search bar
-  // - list_entry: an expected entry in the search results
-  // - heading: the page heading on loading that result
-  // - obs_station: the observation station data for that location in the details section
-  [
-    {
-      term: 'Heathrow',
-      list_entry: 'London Heathrow Airport, Greater London',
-      heading: 'London Heathrow Airport',
-      obs_station: 'Observation Station: London/Heathrow Intl (Lat: 51.4833 | Long: -0.45)',
-    },
-    {
-      term: 'Edinburgh',
-      list_entry: 'Edinburgh, Edinburgh',
-      heading: 'Edinburgh',
-      obs_station: 'Observation Station: Edinburgh/Royal Botanic Garden (Lat: 55.9667 | Long: -3.2167)',
-    },
-  ].forEach(({term, list_entry, heading, obs_station}) => {
-    test('Weather page search returns results for ' + term, async ({ page }) => {
+  // Test data reference
+    // Data imported from ./datasets/weather_full_search.csv
+    // - term: term to be searched in masthead search bar
+    // - list_entry: an expected entry in the search results
+    // - heading: the page heading on loading that result
+    // - obs_station: the observation station data for that location in the details section
+
+  const test_dataset = parse(fs.readFileSync(path.join(__dirname, 'datasets', 'weather_full_search.csv')), {
+    columns: true,
+    skip_empty_lines: true
+  });
+
+  for (const record of test_dataset) {
+    test('Weather page search returns results for ' + record.term, async ({ page }) => {
       // Search for term and get locators
-      const masthead = await input_search_term(page, term);
+      const masthead = await input_search_term(page, record.term);
 
       // Navigate into link
       await masthead.getByRole('button', { name: 'Search', exact: true }).click();
-      await expect(masthead.locator('#location-list').getByRole('link', { name: list_entry })).toBeVisible();
-      await masthead.getByRole('link', { name: list_entry }).click();
+      await expect(masthead.locator('#location-list').getByRole('link', { name: record.list_entry })).toBeVisible();
+      await masthead.getByRole('link', { name: record.list_entry }).click();
       
       // Validate weather page for location
-      await expect(page.getByTestId('location').getByRole('heading', { name: heading })).toBeVisible();
-      await expect(page.locator('.wr-c-observations__details')).toContainText(obs_station);
+      await expect(page.getByTestId('location').getByRole('heading', { name: record.heading })).toBeVisible();
+      await expect(page.locator('.wr-c-observations__details')).toContainText(record.obs_station);
     });
-  })
+  }
+
 });
 
 test.describe('Search for weather forecasts - partial match', () => {
